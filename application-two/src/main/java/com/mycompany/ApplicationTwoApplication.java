dfcb7224175b00c77ff12170a30697e9a5bcbca8
serg10arg <cabrerasergio094@gmail.com>
2025-10-19 16:13:09 -0300

    chore(config): configurar la URI de conexion de MongoDB
    Se establece la cadena de conexion en application.properties para que la
    aplicacion Spring Boot se conecte a la base de datos item-service-db en
    la instancia local de MongoDB.

--- application-two/src/main/java/com/mycompany/ApplicationTwoApplication.java
+++ application-two/src/main/java/com/mycompany/ApplicationTwoApplication.java
@@ -1,4 +1,4 @@
-package com.mycompany.applicationtwo;
+package com.mycompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
