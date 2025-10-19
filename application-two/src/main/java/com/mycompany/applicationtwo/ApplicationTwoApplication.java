161163874f93f8329dfe27cd944d5f5e75be516a
serg10arg <cabrerasergio094@gmail.com>
2025-10-19 12:10:57 -0300

    feat(model): añadir modelos de datos para Item e ItemCategory
    Se crean las clases de entidad para ItemModel y ItemCategory que se
    mapearan a las colecciones correpondientes en MongoDB. Estas clases
    definen la estructura de los productos y sus categorias en la base de
    datos.

---
+++ application-two/src/main/java/com/mycompany/applicationtwo/ApplicationTwoApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.applicationtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTwoApplication.class, args);
+    }
+
+}
